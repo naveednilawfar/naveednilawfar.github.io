4809b8193eda0e4b77ac65e8f526ef02340a57f3
naveednilawfar <naveednilawfar@gmail.com>
2021-09-16 18:24:54 +1000

    Full Update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function MENU() {
+	var js = document.querySelectorAll(".side-bar");
+	js.forEach(m => m.style.display = "block");
+}
+function MENUC() {
+	var js = document.querySelectorAll(".side-bar");
+	js.forEach(m => m.style.display = "none");
+}
