24f675c2c0070fc8aba3e5a3653ef49d71e4148c
Naveed Nilawfar <85788636+naveednilawfar@users.noreply.github.com>
2023-08-31 18:19:12 +1000

    Full Update

--- js/welcome.js
+++
@@ -1,7 +0,0 @@
-$(window).on("load", function() {});
-
-$(".welcome").append("<img src='/img/blackbg.jpg'></img>")
-
-$(".welcome-desktop").addClass("show-on-large")
-$(".welcome-tablet").addClass("show-on-medium")
-$(".welcome-mobile").addClass("show-on-small")
